2aa76a9f3030c09161be2bbb3ab05e4f0d24d78e
Linlin Yan <yanlinlin82@gmail.com>
2015-01-13 15:43:08 +0800

    Add package document.

---
+++ R/Rbionano.R
@@ -0,0 +1,8 @@
+#' Rbionano: R package for BioNano data manipulation and visualization
+#'
+#' @name Rbionano-package
+#' @docType package
+#' @details
+#' Rbionano provides functions to manipulate and visualize BioNano data.
+#'
+NULL
