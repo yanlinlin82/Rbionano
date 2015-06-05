7a54374fe5281fa6c45b3f747ea9212a795de7ce
Linlin Yan <yanlinlin82@gmail.com>
2015-06-05 08:06:56 +0800

    Adapt to smap-v0.4

--- R/read_smap.R
+++ R/read_smap.R
@@ -6,7 +6,11 @@ read_smap <- function(file, sort = FALSE) {
 
 	a <- tbl_df(read.table(file, stringsAsFactors = FALSE))
 
-	if (ncol(a) >= 13) {
+	if (ncol(a) >= 17) {
+		colnames(a) <- c("smapID", "qryID", "refID1", "refID2", "qryStart", "qryEnd",
+				"refStart", "refEnd", "confidence", "type", "xmapID1", "xmapID2",
+				"linkID", "qryStartIdx", "qryEndIdx", "refStartIdx", "refEndIdx")
+	} else if (ncol(a) >= 13) {
 		colnames(a) <- c("smapID", "qryID", "refID1", "refID2", "qryStart", "qryEnd",
 				"refStart", "refEnd", "orientation", "confidence",
 				"type", "xmapID1", "xmapID2")
